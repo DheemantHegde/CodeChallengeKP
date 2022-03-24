1358d5453a0db9926e5e776416200857e6a71a33
dheemanthegde <dheemant.hegde@gmail.com>
2022-03-24 13:45:22 +0530

    Update main.py

--- Challenge#3/main.py
+++ Challenge#3/main.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python
 
-import argparse
-import urllib.request
+def get_object(object, key):
+    for dictkey, dictvalue in object.items():
+        if key == dictkey:
+            return dictvalue
+        else:
+            dictvalue = get_object(dictvalue, key)
+    return dictvalue
 
-# define and parse command-line options
-parser = argparse.ArgumentParser(description='Get AWS Instnace Metadata')
-parser.add_argument('--key', required=False, help='Keyname to get the Value')
-args = vars(parser.parse_args())
+if __name__ == '__main__':
+    object_sample = {"a": {"b": { "c": "d" }}}
 
-url = 'http://169.254.169.254/latest/meta-data'
-keyname = args['key']
-metadata_keys = urllib.request.urlopen(url).read().decode()
-json_hash = {}
+    ## Tests 1 Check for the right dict value
+    val = get_object(object_sample, "c")
+    if val == 'd':
+        print("Test 1 Passed")
+    else:
+        print("Test 1 Failed")
+    
+    # Test 2 Check if the type of returned object is dictionary
+    another_object = {"x": {"y": {"z": "a"}}}
+    val = get_object(another_object, "x")
+    if type(val) is dict:
+        print("Test 2 Passed")
+    else:
+        print("Test 2 Failed")
 
+    # Test 3 check if returned value is expected
+    val = get_object(another_object, "y")
+    if val["z"] == "a":
+        print("Test 3 Passed")
+    else:
+        print("Test 3 Failed")
 
-if keyname:
-    url = "{}/{}".format(url, keyname)
-    response = urllib.request.urlopen(url).read().decode()
-    json_hash[keyname] = response
-    print(json_hash)
-else:
-    for keyname in metadata_keys.split("\n"):
-        response = urllib.request.urlopen(url).read().decode()
-        json_hash[keyname] = response
-        print(json_hash)
+    # Test 4 Check if the returned value is right key of dict
+    val = get_object(another_object, "x")
+    if list(val.keys())[0] == "y":
+        print("Test 4 Passed")
+    else:
+        print("Test 4 Failed")
